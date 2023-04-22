7082042707f642f30489c261b682cfc8d2357baf
Michael Flanakin <micflan@microsoft.com>
2023-04-22 16:33:58 -0700

    Update build script + create Publish script

--- src/scripts/Build-Toolkit.ps1
+++ src/scripts/Build-Toolkit.ps1
@@ -3,38 +3,52 @@
     Builds all toolkit modules and templates for publishing to the Bicep Registry and Azure Quickstart Templates.
 .DESCRIPTION
     Run this from the /src/scripts folder.
+.PARAMETER Template
+    Optional. Name of the module or template to publish. Default = "*" (all templates and modules).
 .EXAMPLE
     ./Build-Toolkit
-    Builds all FinOps toolkit templates.
+    Builds all FinOps toolkit modules and templates.
+.EXAMPLE
+    ./Build-Toolkit -Template "finops-hub"
+    Builds only the finops-hub template.
 #>
 Param(
+    [Parameter(Position = 0)][string]$Template = "*"
 )
 
 # Create output directory
 $outdir = "../../release"
 ./New-Directory $outdir
 
-# Generate Bicep modules
-Get-ChildItem ..\bicep-registry\* -Directory `
+# Generate Bicep Registry modules
+Get-ChildItem ..\bicep-registry\$Template* -Directory -ErrorAction SilentlyContinue `
 | ForEach-Object {
     $module = $_
-    Write-Host "Building module $($module.Name)..."
+    Write-Host "Building Registry module $($module.Name)..."
     ./Build-Bicep $module
     Write-Host ''
 }
 
-# Generate JSON parameters
-Get-ChildItem ..\templates\*\main.bicep `
+# Package Azure Quickstart Template folders
+Get-ChildItem ..\templates\$Template* -Directory -ErrorAction SilentlyContinue `
 | ForEach-Object {
-    $bicep = $_
-    $tmpName = $bicep.Directory.Name
-    $tmpDir = "$outdir/$tmpName"
-    ./New-Directory $tmpDir
+    $srcDir = $_
+    $templateName = $srcDir.Name
 
-    Write-Host "Generating $tmpName template..."
-    bicep build $bicep --outfile "$tmpDir/azuredeploy.json"
-    Write-Host ''
-    Write-Host "Generating $tmpName parameters..."
-    bicep generate-params $bicep --outfile "$tmpDir/azuredeploy.json"
+    Write-Host "Building template $templateName..."
+
+    # Create target directory
+    $destDir = "$outdir/$templateName"
+    ./New-Directory $destDir
+    
+    # Copy required files
+    Write-Host "  Copying files..."
+    Copy-Item "$srcDir/*.*" $destDir
+    Copy-Item "$srcDir/modules" $destDir
+    
+    # Generate parameters
+    Write-Host "  Generating parameters..."
+    bicep generate-params "$srcDir/main.bicep" --outfile "$destDir/azuredeploy.json"
+
     Write-Host ''
 }
