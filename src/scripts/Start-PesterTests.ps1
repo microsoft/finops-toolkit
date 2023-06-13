8276a4def43fdf1af77f2922ea71708f4a16098f
Anthony Romano <aromano@scmpeoc3t.army.mil>
2023-06-13 10:23:53 -0400

    cleanup test start

--- src/scripts/Start-PesterTests.ps1
+++ src/scripts/Start-PesterTests.ps1
@@ -1,3 +1,15 @@
+param
+(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('Unit', 'Meta')]
+    [string]
+    $Type,
+
+    [Parameter()]
+    [switch]
+    $InstallPreReqs
+)
+
 $requiredModules = @(
     @{
         Name    = 'Pester'
@@ -9,42 +21,48 @@ $requiredModules = @(
     }
 )
 
-$repository = Get-PSRepository -Name 'PsGallery'
-if ($repository.InstallationPolicy -ne 'Trusted')
+if ($InstallPreReqs)
 {
-    Set-PSRepository PSGallery -InstallationPolicy 'Trusted'
-}
-
-foreach ($module in $requiredModules)
-{
-    $foundModule = Get-Module -Name $module.Name -ListAvailable
-    if (-not $foundModule -or $foundModule.Version.ToString() -ne $module.Version)
+    $repository = Get-PSRepository -Name 'PsGallery'
+    if ($repository.InstallationPolicy -ne 'Trusted')
     {
-        Install-Module -Name $module.Name -RequiredVersion $module.Version -Force -AllowClobber
+        Set-PSRepository PSGallery -InstallationPolicy 'Trusted'
+    }
+
+    foreach ($module in $requiredModules)
+    {
+        $foundModule = Get-Module -Name $module.Name -ListAvailable
+        if (-not $foundModule -or $foundModule.Version.ToString() -ne $module.Version)
+        {
+            Install-Module -Name $module.Name -RequiredVersion $module.Version -Force -AllowClobber
+        }
     }
 }
 
 Import-Module -Name $requiredModules.Name
 $rootPath = ((Get-Item -Path $PSScriptRoot).Parent.Parent).FullName
-$metaTestPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Meta'
-
-# Run Meta tests
-$pesterArgs = [PesterConfiguration]::Default
-$pesterArgs.Run.Path = $metaTestPath
-$pesterArgs.Output.Verbosity = 'Detailed'
-
-Invoke-Pester -Configuration $pesterArgs
-
-# Run unit tests
-$powerShellPath = Join-Path -Path $rootPath -ChildPath 'src/powershell'
-$unitTestPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Unit'
 
 $pesterArgs = [PesterConfiguration]::Default
-$pesterArgs.Run.Path = $unitTestPath
 $pesterArgs.Output.Verbosity = 'Detailed'
-$pesterArgs.CodeCoverage.Enabled = $true
-$pesterArgs.CodeCoverage.Path = "$powerShellPath/*.ps*1"
-$pesterArgs.CodeCoverage.OutputFormat = 'JaCoCo'
-$pesterArgs.CodeCoverage.OutputPath = "$rootPath/coverage.xml"
+
+switch ($Type)
+{
+    'Meta'
+    {
+        $testPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Meta'
+    }
+
+    'Unit'
+    {
+        $powerShellPath = Join-Path -Path $rootPath -ChildPath 'src/powershell'
+        $testPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Unit'
+        $pesterArgs.CodeCoverage.Enabled = $true
+        $pesterArgs.CodeCoverage.Path = "$powerShellPath/*.ps*1"
+        $pesterArgs.CodeCoverage.OutputFormat = 'JaCoCo'
+        $pesterArgs.CodeCoverage.OutputPath = "$rootPath/coverage.xml"
+    }
+}
+
+$pesterArgs.Run.Path = $testPath
 
 Invoke-Pester -Configuration $pesterArgs
