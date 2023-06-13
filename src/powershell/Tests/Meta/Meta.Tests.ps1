fa956a551b4b6976d36efcbf3b60dbc380941bd0
Anthony Romano <aromano@scmpeoc3t.army.mil>
2023-06-13 10:10:21 -0400

    add script analyzer tests

---
+++ src/powershell/Tests/Meta/Meta.Tests.ps1
@@ -0,0 +1,50 @@
+BeforeDiscovery {
+    $rootPath = 'C:\Repos\cloud-hubs\src\powershell'
+    $filesToInclude = 'FinOpsToolkit.psm1'
+    $files = Get-ChildItem -Path $rootPath -Recurse -Include $filesToInclude
+}
+
+Describe 'Style tests - [<_>]' -ForEach $files.FullName {
+    BeforeDiscovery {
+        $rules = Get-ScriptAnalyzerRule
+    }
+
+    BeforeAll {
+        $file = $_
+
+        function Write-PsScriptAnalyzerWarning
+        {
+            [CmdletBinding()]
+            param
+            (
+                [Parameter(Mandatory = $true)]
+                [string]
+                $FileName,
+
+                [Parameter(Mandatory = $true)]
+                [string]
+                $RuleName,
+
+                [Parameter(Mandatory = $true)]
+                [object[]]
+                $PssaRuleOutput
+            )
+
+            foreach ($rule in $PssaRuleOutput)
+            {
+                Write-Warning -Message "$FileName (Line $($rule.Line)): $($rule.Message)"
+            }
+        }
+    }
+
+    It 'Should pass rule [<_>]' -ForEach $rules.RuleName {
+        $result = Invoke-ScriptAnalyzer -Path $file -IncludeRule $_
+
+        if ($null -ne $result)
+        {
+            Write-PsScriptAnalyzerWarning -FileName $file -RuleName $_ -PssaRuleOutput $result
+        }
+
+        $result | Should -BeNullOrEmpty
+    }   
+}
--- src/scripts/Start-PesterTests.ps1
+++ src/scripts/Start-PesterTests.ps1
@@ -1,17 +1,46 @@
-$pesterModule = Get-Module -Name 'Pester' -ListAvailable
-$pesterVersion = '5.4.1'
-if (-not $pesterModule -or $pesterModule.Version.ToString() -ne $pesterVersion)
+$requiredModules = @(
+    @{
+        Name    = 'Pester'
+        Version = '5.4.1'
+    },
+    @{
+        Name    = 'PSScriptAnalyzer'
+        Version = '1.21.0'
+    }
+)
+
+$repository = Get-PSRepository -Name 'PsGallery'
+if ($repository.InstallationPolicy -ne 'Trusted')
 {
-    Set-PSRepository PSGallery -InstallationPolicy Trusted
-    Install-Module -Name 'Pester' -RequiredVersion $pesterVersion -Force -AllowClobber
+    Set-PSRepository PSGallery -InstallationPolicy 'Trusted'
 }
 
-Import-Module -Name 'Pester'
+foreach ($module in $requiredModules)
+{
+    $foundModule = Get-Module -Name $module.Name -ListAvailable
+    if (-not $foundModule -or $foundModule.Version.ToString() -ne $module.Version)
+    {
+        Install-Module -Name $module.Name -RequiredVersion $module.Version -Force -AllowClobber
+    }
+}
+
+Import-Module -Name $requiredModules.Name
 $rootPath = ((Get-Item -Path $PSScriptRoot).Parent.Parent).FullName
+$metaTestPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Meta'
+
+# Run Meta tests
+$pesterArgs = [PesterConfiguration]::Default
+$pesterArgs.Run.Path = $metaTestPath
+$pesterArgs.Output.Verbosity = 'Detailed'
+
+Invoke-Pester -Configuration $pesterArgs
+
+# Run unit tests
 $powerShellPath = Join-Path -Path $rootPath -ChildPath 'src/powershell'
+$unitTestPath = Join-Path -Path $rootPath -ChildPath 'src/powershell/Tests/Unit'
 
 $pesterArgs = [PesterConfiguration]::Default
-$pesterArgs.Run.Path = $powerShellPath
+$pesterArgs.Run.Path = $unitTestPath
 $pesterArgs.Output.Verbosity = 'Detailed'
 $pesterArgs.CodeCoverage.Enabled = $true
 $pesterArgs.CodeCoverage.Path = "$powerShellPath/*.ps*1"
