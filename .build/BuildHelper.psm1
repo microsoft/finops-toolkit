49fe4792f2c928cadab2979fca8ae12ceb913def
Anthony Romano <aromano@scmpeoc3t.army.mil>
2023-08-15 11:18:38 -0400

    cleanup

--- .build/BuildHelper.psm1
+++ .build/BuildHelper.psm1
@@ -1,5 +1,4 @@
 $functionPath = Join-Path -Path $PSScriptRoot -ChildPath 'BuildHelper/*.ps1'
-$functionPath
 $functions = Get-ChildItem -Path $functionPath -Filter '*.ps1'
 foreach ($function in $functions)
 {
