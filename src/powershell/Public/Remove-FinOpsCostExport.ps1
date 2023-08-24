6c61cc62ddb836aa558e261d48034acdf32bf98a
Sacha Narinx <Springstone@users.noreply.github.com>
2023-08-24 23:05:20 +0400

    Fixing basic issues. Should be good now.

--- src/powershell/Public/Remove-FinOpsCostExport.ps1
+++ src/powershell/Public/Remove-FinOpsCostExport.ps1
@@ -30,7 +30,8 @@
         Deletes a Cost Management Export named MyExport scoped to /subscriptions/00000000-0000-0000-0000-000000000000, and deletes all data associated with that scope.
 #>
 
-function Remove-FinOpsCostExport {
+function Remove-FinOpsCostExport
+{
   [CmdletBinding(SupportsShouldProcess)]
   param
   (
@@ -51,16 +52,38 @@ function Remove-FinOpsCostExport {
     $APIVersion = '2023-04-01-preview'
   )
 
-  try {
-
-    # TODO : Validate parameters and resources before proceeding
-
+  try
+  {
     $additionalParamaters = @{
       scope = $scope
     }
     $payload = ConvertTo-Json -InputObject $additionalParamaters -Depth 100
 
-    if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport')) {
+    # TODO : Validate parameters and resources before proceeding
+    $httpResponse = Invoke-AzRestMethod `
+      -ResourceProviderName "Microsoft.CostManagement" `
+      -ResourceType "exports" `
+      -Name $Name `
+      -ApiVersion $ApiVersion `
+      -Method "GET" `
+      -Payload $payload
+
+    if ($httpResponse.StatusCode -eq 200)
+    {
+      # Export deleted successfully
+    }
+    elseif ($httpResponse.StatusCode -eq 404)
+    {
+      # Not found
+    }
+    else
+    {
+      # Error response describing why the operation failed.
+      throw "Not Exists: Cost Management Export operation failed with message: '$($httpResponse.Content)'"
+    }
+
+    if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport'))
+    {
 
       # Using the REST API to delete the export as requested as PS modules are outdated?
       $httpResponse = Invoke-AzRestMethod `
@@ -71,17 +94,24 @@ function Remove-FinOpsCostExport {
         -Method "DELETE" `
         -Payload $payload
 
-      if ($httpResponse.StatusCode -eq 200) {
+      if ($httpResponse.StatusCode -eq 200)
+      {
         # Export deleted successfully
-      } else {
+      }
+      elseif ($httpResponse.StatusCode -eq 404)
+      {
+        # Not found
+      }
+      else
+      {
         # Error response describing why the operation failed.
         throw "Delete Cost Management Export operation failed with message: '$($httpResponse.Content)'"
       }
-
     }
 
     # Delete associated ingestion data from storage account
-    if ($RemoveData) {
+    if ($RemoveData)
+    {
       # Using the REST API to get the export as requested as PS modules are outdated?
       $httpResponse = Invoke-AzRestMethod `
         -ResourceProviderName "Microsoft.CostManagement" `
@@ -91,33 +121,49 @@ function Remove-FinOpsCostExport {
         -Method "GET" `
         -Payload $payload
 
-      if ($httpResponse.StatusCode -eq 200) {
+      if ($httpResponse.StatusCode -eq 200)
+      {
 
         # Export details retreived
         $exportDetails = ConvertFrom-Json -InputObject $httpResponse.Content
         $storageAccountID = $exportDetails.properties.deliveryInfo.destination.resourceId
-        # $container = "ingestion"
-        # $rootFolder = $scope
-        # $type = $exportDetails.properties.definition.type
 
         # Get-AzStorageAccount -resourcegroupname alz-finopstk -name finopshub255k7ov6asado | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path "/" -Recurse -FetchProperty | Where-Object IsDirectory -ne false | Select-Object Path
         $resourceGroupName = $storageAccountID.Split('/')[4]
         $storageAccountName = $storageAccountID.Split('/')[8]
 
         #Hold on to your hats, deleting all the files in the ingestion scope. Using Az PS module as this should be current.
-        if ($PSCmdlet.ShouldProcess($scope, 'DeleteCostReports')){
-          Get-AzStorageAccount -resourcegroupname $resourceGroupName -name $storageAccountName | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path $scope -Recurse -FetchProperty | Remove-AzDataLakeGen2Item -Force
-        }
+        if ($PSCmdlet.ShouldProcess($scope, 'DeleteCostReports'))
+        {
+          Write-Verbose "Resourace Group: $resourceGroupName"
+          Write-Verbose "Storage Account: $storageAccountName"
+          Write-Verbose "Scope: $scope"
 
-      } else {
+          $getFiles = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path $scope -Recurse -FetchProperty
+          if ($getFiles.Count -gt 0)
+          {
+            $getFiles | Remove-AzDataLakeGen2Item -Force
+          }
+        }
+      }
+      elseif ($httpResponse.StatusCode -eq 404)
+      {
+        # Not found
+      }
+      else
+      {
         # Error response describing why the operation failed.
         throw "Delete ingestion data operation failed with message: '$($httpResponse.Content)'"
       }
     }
 
-  } catch {
+  }
+  catch
+  {
     throw $_.Exception.Message
-  } finally {
+  }
+  finally
+  {
     # Nothing to do here
   }
 }
