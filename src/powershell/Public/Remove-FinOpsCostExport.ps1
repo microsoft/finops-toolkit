a66acb976d5c6e133a916b1094b4e0833d201695
Sacha Narinx <Springstone@users.noreply.github.com>
2023-08-24 18:14:29 +0400

    PIT Dev work

--- src/powershell/Public/Remove-FinOpsCostExport.ps1
+++ src/powershell/Public/Remove-FinOpsCostExport.ps1
@@ -1,33 +1,84 @@
 
-function Remove-FinOpsCostExport
-{
+function Remove-FinOpsCostExport {
   [CmdletBinding(SupportsShouldProcess)]
   param
   (
-      [Parameter(Mandatory = $true)]
-      [string]
-      $Name,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Name,
 
-      [Parameter(Mandatory = $true)]
-      [string]
-      $Scope,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Scope,
 
-      [Parameter]
-      [switch]
-      $RemoveData,
+    [Parameter]
+    [switch]
+    $RemoveData,
 
-      [Parameter()]
-      [string]
-      $ApiVersion = '2023-04-01-preview'
+    [Parameter()]
+    [string]
+    $ApiVersion = '2023-04-01-preview'
   )
 
   try {
 
-  }
-  catch {
-    <#Do this if a terminating exception happens#>
-  }
-  finally {
+    # TODO : Validate parameters
+
+    $additionalParamaters = @{
+      scope = $scope
+    }
+    $payload = ConvertTo-Json -InputObject $additionalParamaters -Depth 100
+
+    if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport')) {
+
+      $httpResponse = Invoke-AzRestMethod `
+        -ResourceProviderName "Microsoft.CostManagement" `
+        -ResourceType "exports" `
+        -Name $Name `
+        -ApiVersion $ApiVersion `
+        -Method "DELETE" `
+        -Payload $payload
+
+      if ($httpResponse.StatusCode -eq 200) {
+        # Export deleted successfully
+      } else {
+        # Error response describing why the operation failed.
+        throw "Operation failed with message: '$($httpResponse.Content)'"
+      }
+
+    }
+
+    if ($RemoveData) {
+      # Delete associated ingestion data from storage account
+      $httpResponse = Invoke-AzRestMethod `
+        -ResourceProviderName "Microsoft.CostManagement" `
+        -ResourceType "exports" `
+        -Name $Name `
+        -ApiVersion $ApiVersion `
+        -Method "GET" `
+        -Payload $payload
+
+      if ($httpResponse.StatusCode -eq 200) {
+
+        # Export details retreived
+        $exportDetails = ConvertFrom-Json -InputObject $httpResponse.Content
+        $storageAccountID = $exportDetails.properties.deliveryInfo.destination.resourceId
+        $container = "ingestion"
+        $rootFolder = $scope
+        $type = $exportDetails.properties.definition.type
+
+        # Get-AzStorageAccount -resourcegroupname alz-finopstk -name finopshub255k7ov6asado | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path "/" -Recurse -FetchProperty | Where-Object IsDirectory -ne false | Select-Object Path
+
+
+      } else {
+        # Error response describing why the operation failed.
+        throw "Operation failed with message: '$($httpResponse.Content)'"
+      }
+    }
+
+  } catch {
+    throw $_.Exception.Message
+  } finally {
     <#Do this after the try block regardless of whether an exception occurred or not#>
   }
 }
