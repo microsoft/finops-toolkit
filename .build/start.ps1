ec7de3275c6644fdbc05da88d3cce99e916c4807
Anthony Romano <26576969+aromano2@users.noreply.github.com>
2023-08-24 18:44:05 -0400

    Merge recent changes from Dev (#261)
    Co-authored-by: Anthony Romano <aromano@scmpeoc3t.army.mil>

---
+++ .build/start.ps1
@@ -0,0 +1,43 @@
+
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('PreRequisites', 'Build.Module', 'Publish.Module', 'Test.Unit', 'Test.Meta', 'Test.All')]
+    [string[]]
+    $Task,
+
+    [Parameter()]
+    [string]
+    $Version,
+
+    [Parameter()]
+    [ValidateSet('alpha', 'preview')]
+    [string]
+    $PrereleaseTag
+)
+
+if (-not (Get-Module -Name 'PsDepend' -ListAvailable))
+{
+    $repository = Get-PSRepository -Name 'PSGallery'
+    if ($repository.InstallationPolicy -ne 'Trusted')
+    {
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    }
+
+    try
+    {
+        Install-Module -Name 'PsDepend' -Force -AllowClobber -ErrorAction 'Stop'
+    }
+    catch
+    {
+        throw $_
+    }
+}
+
+$buildPath = Join-Path -Path $PSScriptRoot -ChildPath 'build.ps1'
+$dependencyPath = Get-ChildItem -Path $PSScriptRoot -Filter '*.depends.psd1'
+Invoke-PSDepend -Path $dependencyPath.FullName -Install -Import -Force
+
+$buildPath
+Invoke-Build -File $buildPath @PSBoundParameters
