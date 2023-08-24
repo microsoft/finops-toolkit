5d490f78112da045b7f4968518bdb20956c1c249
Sacha Narinx <Springstone@users.noreply.github.com>
2023-08-24 18:50:33 +0400

    .

--- src/powershell/Public/Remove-FinOpsCostExport.ps1
+++ src/powershell/Public/Remove-FinOpsCostExport.ps1
@@ -31,6 +31,7 @@ function Remove-FinOpsCostExport {
 
     if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport')) {
 
+      # Using the REST API to delete the export as requested as PS modules are outdated?
       $httpResponse = Invoke-AzRestMethod `
         -ResourceProviderName "Microsoft.CostManagement" `
         -ResourceType "exports" `
@@ -50,6 +51,7 @@ function Remove-FinOpsCostExport {
 
     if ($RemoveData) {
       # Delete associated ingestion data from storage account
+      # Using the REST API to delete the export as requested as PS modules are outdated?
       $httpResponse = Invoke-AzRestMethod `
         -ResourceProviderName "Microsoft.CostManagement" `
         -ResourceType "exports" `
@@ -63,12 +65,16 @@ function Remove-FinOpsCostExport {
         # Export details retreived
         $exportDetails = ConvertFrom-Json -InputObject $httpResponse.Content
         $storageAccountID = $exportDetails.properties.deliveryInfo.destination.resourceId
-        $container = "ingestion"
-        $rootFolder = $scope
-        $type = $exportDetails.properties.definition.type
+        # $container = "ingestion"
+        # $rootFolder = $scope
+        # $type = $exportDetails.properties.definition.type
 
         # Get-AzStorageAccount -resourcegroupname alz-finopstk -name finopshub255k7ov6asado | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path "/" -Recurse -FetchProperty | Where-Object IsDirectory -ne false | Select-Object Path
+        $resourceGroupName = $storageAccountID.Split('/')[4]
+        $storageAccountName = $storageAccountID.Split('/')[8]
 
+        #Hold on to your hats, deleting all the files in the ingestion scope
+        Get-AzStorageAccount -resourcegroupname $resourceGroupName -name $storageAccountName | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path $scope -Recurse -FetchProperty | Remove-AzDataLakeGen2Item -Force
 
       } else {
         # Error response describing why the operation failed.
@@ -79,6 +85,6 @@ function Remove-FinOpsCostExport {
   } catch {
     throw $_.Exception.Message
   } finally {
-    <#Do this after the try block regardless of whether an exception occurred or not#>
+    # Nothing to do here
   }
 }
