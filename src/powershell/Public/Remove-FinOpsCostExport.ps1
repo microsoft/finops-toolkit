bc61c9d657382349da3bb64ab81c40b48638ca90
Sacha Narinx <Springstone@users.noreply.github.com>
2023-08-28 15:35:54 +0400

    Cleanup

--- src/powershell/Public/Remove-FinOpsCostExport.ps1
+++ src/powershell/Public/Remove-FinOpsCostExport.ps1
@@ -85,7 +85,6 @@ function Remove-FinOpsCostExport
         $storageAccountName = $storageAccountID.Split('/')[8]
         $path = $scope + "/" + $Name
 
-        # Hold on to your hats, deleting all the files in the ingestion scope. Using Az PS module as this should be current.
         if ($PSCmdlet.ShouldProcess($scope, 'DeleteCostReports'))
         {
           Write-Verbose "Resource group: $resourceGroupName"
@@ -94,8 +93,6 @@ function Remove-FinOpsCostExport
           Write-Verbose "Path: $path"
 
           $getSta = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
-          # $getFiles = $getSta | Get-AzDataLakeGen2ChildItem -FileSystem "msexports" -Path $path -Recurse -FetchProperty
-          # if (($getFiles.Count -gt 0) -and $getSta.EnableHierarchicalNamespace)
           if ($getSta.EnableHierarchicalNamespace)
           {
             $getSta | Remove-AzDataLakeGen2Item -FileSystem "msexports" -Path $path -Force
@@ -106,13 +103,11 @@ function Remove-FinOpsCostExport
 
     if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport'))
     {
-      # Using the REST API to delete the export as requested as PS modules are outdated?
       $httpResponse = Invoke-AzRestMethod @invokeAzRestMethodParams -Method "DELETE"
 
       if ($httpResponse.StatusCode -eq 404) { break }
       elseif ($httpResponse.StatusCode -ne 200)
       {
-        # Error response describing why the operation failed.
         throw ($script:localizedData.DeleteCostExportFailed -f $($httpResponse.Content))
       }
     }
