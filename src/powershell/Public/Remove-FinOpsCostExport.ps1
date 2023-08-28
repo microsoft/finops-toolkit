4ed8e5e5ab929318597c2dd897b49ac18b9acb88
Sacha Narinx <Springstone@users.noreply.github.com>
2023-08-28 13:40:25 +0400

    Additional changes as per review

--- src/powershell/Public/Remove-FinOpsCostExport.ps1
+++ src/powershell/Public/Remove-FinOpsCostExport.ps1
@@ -58,14 +58,17 @@ function Remove-FinOpsCostExport
     }
     $payload = ConvertTo-Json -InputObject $additionalParamaters -Depth 100
 
+    $invokeAzRestMethodParams = @{
+      ResourceProviderName = "Microsoft.CostManagement"
+      ResourceType = "exports"
+      Name = $Name
+      ApiVersion = $ApiVersion
+      Method = "GET"
+      Payload = $payload
+    }
+
     # TODO : Validate parameters and resources before proceeding
-    $httpResponse = Invoke-AzRestMethod `
-      -ResourceProviderName "Microsoft.CostManagement" `
-      -ResourceType "exports" `
-      -Name $Name `
-      -ApiVersion $ApiVersion `
-      -Method "GET" `
-      -Payload $payload
+    $httpResponse = Invoke-AzRestMethod @invokeAzRestMethodParams
 
     if ($httpResponse.StatusCode -eq 404) { break }
     elseif ($httpResponse.StatusCode -ne 200)
@@ -75,15 +78,8 @@ function Remove-FinOpsCostExport
 
     if ($PSCmdlet.ShouldProcess($Name, 'DeleteCostExport'))
     {
-
       # Using the REST API to delete the export as requested as PS modules are outdated?
-      $httpResponse = Invoke-AzRestMethod `
-        -ResourceProviderName "Microsoft.CostManagement" `
-        -ResourceType "exports" `
-        -Name $Name `
-        -ApiVersion $ApiVersion `
-        -Method "DELETE" `
-        -Payload $payload
+      $httpResponse = Invoke-AzRestMethod @invokeAzRestMethodParams -Method "DELETE"
 
       if ($httpResponse.StatusCode -eq 404) { break }
       elseif ($httpResponse.StatusCode -ne 200)
@@ -97,17 +93,10 @@ function Remove-FinOpsCostExport
     if ($RemoveData)
     {
       # Using the REST API to get the export as requested as PS modules are outdated?
-      $httpResponse = Invoke-AzRestMethod `
-        -ResourceProviderName "Microsoft.CostManagement" `
-        -ResourceType "exports" `
-        -Name $Name `
-        -ApiVersion $ApiVersion `
-        -Method "GET" `
-        -Payload $payload
+      $httpResponse = Invoke-AzRestMethod @invokeAzRestMethodParams
 
       if ($httpResponse.StatusCode -eq 200)
       {
-
         # Export details retreived
         $exportDetails = ConvertFrom-Json -InputObject $httpResponse.Content
         $storageAccountId = $exportDetails.properties.deliveryInfo.destination.resourceId
@@ -116,14 +105,14 @@ function Remove-FinOpsCostExport
         $resourceGroupName = $storageAccountID.Split('/')[4]
         $storageAccountName = $storageAccountID.Split('/')[8]
 
-        #Hold on to your hats, deleting all the files in the ingestion scope. Using Az PS module as this should be current.
+        # Hold on to your hats, deleting all the files in the ingestion scope. Using Az PS module as this should be current.
         if ($PSCmdlet.ShouldProcess($scope, 'DeleteCostReports'))
         {
           Write-Verbose "Resource group: $resourceGroupName"
           Write-Verbose "Storage account: $storageAccountName"
           Write-Verbose "Scope: $scope"
 
-          $getFiles = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path $scope -Recurse -FetchProperty
+          $getFiles = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName | Get-AzDataLakeGen2ChildItem -FileSystem "ingestion" -Path $scope -Recurse -FetchProperty # TODO: verify folder/files to delete (ingestion or msexports)
           if ($getFiles.Count -gt 0)
           {
             $getFiles | Remove-AzDataLakeGen2Item -Force
@@ -137,10 +126,9 @@ function Remove-FinOpsCostExport
       else
       {
         # Error response describing why the operation failed.
-        throw "Delete ingestion data operation failed with message: '$($httpResponse.Content)'"
+        throw ($script:localizedData.DeleteCostExportFilesFailed -f $($httpResponse.Content))
       }
     }
-
   }
   catch
   {
