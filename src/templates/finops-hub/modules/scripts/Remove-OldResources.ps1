75d91f8f579a97ea0dd212d08e37c4f124c3d2e6
Michael Flanakin <micflan@microsoft.com>
2023-09-09 22:18:28 -0700

    Delete old triggers/pipelines

---
+++ src/templates/finops-hub/modules/scripts/Remove-OldResources.ps1
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Init outputs
+$DeploymentScriptOutputs = @{}
+
+# 
+$adfParams = @{
+    ResourceGroupName = $env:DataFactoryResourceGroup
+    DataFactoryName   = $env:DataFactoryName
+}
+
+# Delete old triggers
+$triggers = Get-AzDataFactoryV2Trigger @adfParams -ErrorAction SilentlyContinue `
+| Where-Object { $_.Name -match '^msexports(_(setup|daily|monthly|extract))?$' }
+$DeploymentScriptOutputs["stopTriggers"] = $triggers | Stop-AzDataFactoryV2Trigger -Force -ErrorAction SilentlyContinue
+$DeploymentScriptOutputs["deleteTriggers"] = $triggers | Remove-AzDataFactoryV2Trigger -Force -ErrorAction SilentlyContinue
+
+# Delete old pipelines
+$DeploymentScriptOutputs["pipelines"] = Get-AzDataFactoryV2Pipeline @adfParams -ErrorAction SilentlyContinue `
+| Where-Object { $_.Name -match '^msexports_(backfill|extract|fill|get|run|setup|transform)$' } `
+| Remove-AzDataFactoryV2Pipeline -Force -ErrorAction SilentlyContinue
