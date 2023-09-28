48f1a1a20624bacc0b4e217d913ac470bb4bc3cc
Michael Flanakin <micflan@microsoft.com>
2023-09-28 10:39:37 -0700

    Bring version back

--- src/scripts/Package-Toolkit.ps1
+++ src/scripts/Package-Toolkit.ps1
@@ -48,18 +48,33 @@ if ($Template -ne "*" -and -not (Test-Path $relDir)) {
 Write-Host "Packaging templates..."
 
 # Package files for release
-$version = Invoke-Task Version
+$version = ./Invoke-Task Version
 Remove-Item "$relDir/*-$version.zip" -Force
-Get-ChildItem $relDir `
+Get-ChildItem $relDir -Directory `
 | ForEach-Object {
+    Write-Verbose "Packaging $_"
+    $path = $_
+    $versionSubFolder = (Join-Path $path $version)
+    $zip = Join-Path $relDir "$($path.Name)-$version.zip"
+
+    Write-Verbose "Checking for a nested version folder: $versionSubFolder"
+    if ((Test-Path -Path $versionSubFolder -PathType Container) -eq $true) {
+        Write-Verbose "  Switching to sub folder"
+        $path = $versionSubFolder
+    }
+    
     # Skip if template is a Bicep Registry module
-    if (Test-Path $_/version.json) {
-        $versionSchema = (Get-Content "$_\version.json" -Raw | ConvertFrom-Json | Select-Object -ExpandProperty '$schema')
+    Write-Verbose "Checking version.json to see if it's targeting the Bicep Registry"
+    if (Test-Path $path/version.json) {
+        $versionSchema = (Get-Content "$path\version.json" -Raw | ConvertFrom-Json | Select-Object -ExpandProperty '$schema')
         if ($versionSchema -like '*bicep-registry-module*') {
+            Write-Path "Skipping Bicep Registry module (not included in releases)"
             return;
         }
     }
-    Compress-Archive -Path "$_/*" -DestinationPath "$relDir/$($_.Name)-$version.zip"
+
+    Write-Verbose "Compressing $path to $zip"
+    Compress-Archive -Path "$path/*" -DestinationPath $zip
 }
 
 Write-Host ''
