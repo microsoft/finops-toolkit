e1979bbfd906e8cda55604ae0ec5e37a0b57ac74
Anthony Romano <26576969+aromano2@users.noreply.github.com>
2023-10-06 03:50:44 -0400

    Fix PowerShell Publish (#389)

--- .build/start.ps1
+++ .build/start.ps1
@@ -40,22 +40,24 @@ param
     $Version
 )
 
-if (-not (Get-Module -Name 'PsDepend' -ListAvailable)) {
-    $repository = Get-PSRepository -Name 'PSGallery'
-    if ($repository.InstallationPolicy -ne 'Trusted') {
-        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+$buildPath = Join-Path -Path $PSScriptRoot -ChildPath 'build.ps1'
+if (-not $env:CI)
+{
+    if (-not (Get-Module -Name 'PsDepend' -ListAvailable)) {
+        $repository = Get-PSRepository -Name 'PSGallery'
+        if ($repository.InstallationPolicy -ne 'Trusted') {
+            Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        }
+    
+        try {
+            Install-Module -Name 'PsDepend' -Force -AllowClobber -Scope CurrentUser -ErrorAction 'Stop'
+        } catch {
+            throw $_
+        }
     }
 
-    try {
-        Install-Module -Name 'PsDepend' -Force -AllowClobber -Scope CurrentUser -ErrorAction 'Stop'
-    } catch {
-        throw $_
-    }
+    $dependencyPath = Get-ChildItem -Path $PSScriptRoot -Filter '*.depends.psd1'
+    Invoke-PSDepend -Path $dependencyPath.FullName -Install -Import -Force
 }
 
-$buildPath = Join-Path -Path $PSScriptRoot -ChildPath 'build.ps1'
-$dependencyPath = Get-ChildItem -Path $PSScriptRoot -Filter '*.depends.psd1'
-Invoke-PSDepend -Path $dependencyPath.FullName -Install -Import -Force
-
-$buildPath
 Invoke-Build -Task $Task -File $buildPath -TaskParams $PSBoundParameters -Verbose:$VerbosePreference
