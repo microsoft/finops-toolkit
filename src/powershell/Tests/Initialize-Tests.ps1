62f3d5f852cf4113e80ce7bbf67d04e735311290
Michael Flanakin <flanakin@users.noreply.github.com>
2023-10-29 12:32:19 -0700

    Separate integration tests (#443)

---
+++ src/powershell/Tests/Initialize-Tests.ps1
@@ -0,0 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Remove-Module FinOpsToolkit -ErrorAction SilentlyContinue
+Import-Module -FullyQualifiedName "$PSScriptRoot/../FinOpsToolkit.psm1"
