86602d3dc787c8d7262b04dc56944fdb53702b08
Michael Flanakin <micflan@microsoft.com>
2024-01-14 14:51:38 -0800

    Expand exports integration test

--- src/powershell/Tests/Integration/CostExports.Tests.ps1
+++ src/powershell/Tests/Integration/CostExports.Tests.ps1
@@ -5,15 +5,41 @@
 
 Describe 'CostExports' {
     It 'Create-Read-Update-Delete exports' -Skip {
-        # TODO: Add integration tests after the New-FinOpsCostExport is in
-    }
-
-    It 'Should get all exports' {
         # Arrange
-        # Act
-        $result = Get-FinOpsCostExport
-
+        $context = Get-AzContext
+        $scope = "/subscriptions/$($context.Subscription.Id)"
+        $storage = New-AzStorageAccount `
+            -ResourceGroupName "ftk-integration-tests" `
+            -Name "ftkintcostexports$($context.Subscription.Id.Replace("-", ""))" `
+            -Location "East US" `
+            -SkuName Standard_LRS
+        $name = "ftk-int-CostExports"
+    
+        # Act -- create
+        $newResult = New-FinOpsCostExport -Name $name -Scope $scope -StorageAccountId $storage.Id
+    
         # Assert
-        $result.Count | Should -BeGreaterThan 0
+        $newResult | Should -Not -BeNull
+
+        # Act -- read
+        $getResult = Get-FinOpsCostExport -Name $name -Scope $scope
+        
+        # Assert
+        $getResult.Count | Should -Be 1
+        $getResult.Name | Should -Be $name
+        
+        # Act -- run now
+        $runResult = Start-FinOpsCostExport -Name $name -Scope $scope
+        
+        # Assert
+        $runResult | Should -Not -BeNull
+        
+        # Act -- delete
+        $deleteResult = Remove-FinOpsCostExport -Name $name -Scope $scope
+        $confirmDeleteResult = Get-FinOpsCostExport -Name $name -Scope $scope
+        
+        # Assert
+        $deleteResult | Should -Not -BeNull
+        $confirmDeleteResult | Should -BeNull
     }
 }
