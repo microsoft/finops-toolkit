c3fc4e79e30e62679561e4446e738bf42a60897f
Michael Flanakin <micflan@microsoft.com>
2024-01-16 00:32:04 -0800

    Update Invoke-Rest to use telemetry headers

--- src/powershell/Private/Invoke-Rest.ps1
+++ src/powershell/Private/Invoke-Rest.ps1
@@ -3,68 +3,102 @@
 
 <#
     .SYNOPSIS
-    Runs Invoke-AzRestMethod and provides output with error handling.
-
-    .PARAMETER Path
-    Path of target resource URL. Hostname of Resource Manager should not be added.
+    Executes an HTTP REST API and provides output with error handling.
 
     .PARAMETER Method
-    Http Method to invoke. Accepted values: GET, POST, PUT, PATCH, DELETE
-    
-    .PARAMETER Payload
-    JSON format payload to pass with the request.
+    Required. HTTP Method to invoke. Accepted values: GET, POST, PUT, PATCH, DELETE
+
+    .PARAMETER Uri
+    Required. URI of target resource or operation. Do not include the Azure Resource Manager domain.
+
+    .PARAMETER Body
+    Optional. HTTP request body.
+
+    .PARAMETER CommandName
+    Required. Name of the PowerShell command being run. This is included in backend telemetry.
+
+    .PARAMETER ParameterSetName
+    Optional. Name of the PowerShell parameter set being used. This is included in backend telemetry.
 
     .EXAMPLE
-    Invoke-Rest -Path "subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/providers/Microsoft.CostManagement/exports/August2023OneTime?api-version=2023-08-01" -Method GET
-    Invoke GET method against a target resource url.
+    Invoke-Rest -Method GET -Uri "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/providers/Microsoft.CostManagement/exports/August2023OneTime?api-version=2023-08-01"
+
+    Invoke GET method against a target resource URI.
 #>
 function Invoke-Rest
 {
     [CmdletBinding()]
     param
     (
-        [Parameter(Mandatory = $true)]
-        [string]
-        $Path,
-
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, Position = 0)]
         [ValidateSet("GET", "POST", "PUT", "PATCH", "DELETE")]
         [string]
         $Method,
         
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $true, Position = 1)]
         [string]
-        $Payload
+        $Uri,
+        
+        [Parameter()]
+        [string]
+        $Body,
+        
+        [Parameter(Mandatory = $true)]
+        [string]
+        $CommandName,
+
+        [Parameter()]
+        [string]
+        $ParameterSetName
     )
-    
+
     $VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
-    $ErrorActionPreference = $PSCmdlet.GetVariableValue('ErrorActionPreference') 
-    Write-Verbose "Invoking $path, Method $Method Payload $Payload`n"
+    $ErrorActionPreference = $PSCmdlet.GetVariableValue('ErrorActionPreference')
+    
+    $arm = (Get-AzContext).Environment.ResourceManagerUrl
+    $params = @{
+        Method      = $Method
+        Uri         = $arm.Trim('/') + '/' + $Uri.Trim('/')
+        Headers     = @{
+            Authorization             = "Bearer $((Get-AzAccessToken).Token)"
+            ClientType                = 'FinOpsToolkit'
+            "Content-Type"            = 'application/json'
+            "x-ms-command-name"       = $CommandName
+            "x-ms-parameter-set-name" = $ParameterSetName
+        }
+        ErrorAction = "Stop"
+    }
+    
+    Write-Verbose "Invoking $Method $fullUri with request body $Body`n"
 
     try
     {
-        if ($Payload) 
+        $response = Invoke-WebRequest @params -Body $Body
+        $content = $response.Content | ConvertFrom-Json -Depth 100
+    }
+    catch
+    {
+        $response = $_.Exception.Response
+        $content = $_.ErrorDetails.Message | ConvertFrom-Json -Depth 10
+        if ($content.error)
         {
-            $httpResponse = Invoke-AzRestMethod -Path $Path -Method $Method -Payload $Payload -ErrorAction stop
+            $errorCode = $content.error.code
+            $errorMessage = $content.error.message
+            Write-Error -Message $($script:localizedData.Common_ErrorResponse -f $errorMessage, $errorCode)
         }
         else
         {
-            $httpResponse = Invoke-AzRestMethod -Path $Path -Method $Method -ErrorAction stop
-        }
-    
-        if ($httpResponse -and $($httpResponse.Content | ConvertFrom-Json).error)
-        {
-            $errorobject = $($httpResponse.Content | ConvertFrom-Json).error
-            $errorcode = $errorobject.code
-            $errorcodemessage = $errorobject.message
-            Write-Error -Message $($script:localizedData.ErrorResponse -f $errorcodemessage, $errorcode)
-        }
-    return $httpResponse
-    }
-    catch 
-        { 
             throw $_.Exception.Message
         }
+    }
+    return @{
+        Headers    = $response.Headers
+        StatusCode = $response.StatusCode
+        Success    = $response.StatusCode -ge 200 -and $response.StatusCode -lt 300
+        Failure    = $response.StatusCode -ge 300
+        NotFound   = $response.StatusCode -eq 404 -or $response.StatusCode -eq 'NotFound'
+        Content    = $content
+    }
 }
 
 
