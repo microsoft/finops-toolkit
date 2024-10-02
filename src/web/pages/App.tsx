f8b585b75e4587a4e5a6cf44876fab33dfb9e7a7
Orthodoxos Kipouridis <kipouridis@gmx.de>
2024-10-02 18:30:23 +0200

    Update src/web/pages/App.tsx
    Co-authored-by: Michael Flanakin <flanakin@users.noreply.github.com>

--- src/web/pages/App.tsx
+++ src/web/pages/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <h2>Automate and extend the Microsoft Cloud with starter kits, scripts, and advanced solutions to accelerate your FinOps journey.</h2>
       </head>
       <p>
-          Marketing Site for the FinOps Toolkit
+          The Microsoft FinOps toolkit is an open-source collection of learning resources and customizable tools to help you adopt and implement FinOps capabilities that automate and extend the Microsoft Cloud. This includes:
         </p>
         <p className="read-the-docs">
         Learn more at <a href="https://microsoft.github.io/finops-toolkit/">Official FinOps toolkit Site</a>
