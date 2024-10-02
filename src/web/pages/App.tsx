3a810af434b49de0f673da1ad6f356176108547f
Orthodoxos Kipouridis <kipouridis@gmx.de>
2024-10-02 18:30:15 +0200

    Update src/web/pages/App.tsx
    Co-authored-by: Michael Flanakin <flanakin@users.noreply.github.com>

--- src/web/pages/App.tsx
+++ src/web/pages/App.tsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      <h1>FinOps toolkit </h1>
+      <head>
+        <h1>Kick start your FinOps efforts</h1>
+        <h2>Automate and extend the Microsoft Cloud with starter kits, scripts, and advanced solutions to accelerate your FinOps journey.</h2>
+      </head>
       <p>
           Marketing Site for the FinOps Toolkit
         </p>
