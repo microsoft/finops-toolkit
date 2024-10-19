505616408e57e5ff41f34372ec229689dff0cac6
Jamel Achahbar <jachahbar@microsoft.com>
2024-10-19 14:12:46 +0200

    changed order of pages and linknames

--- src/web/pages/App.tsx
+++ src/web/pages/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
-import ToolsLandingPage from './ToolsLandingPage';
 import FinOpsHubsPage from './FinOpsHubsPage';
 import FinOpsWorkbooksPage from './FinOpsWorkbooksPage';
 import GovernanceWorkbookPage from './GovernanceWorkbookPage';
@@ -10,26 +9,27 @@ import BicepRegistryModulesPage from './BicepRegistryModulesPage';
 import OpenDataPage from './OpenDataPage';
 import PowerBIReportsPage from './PowerBIReportsPage';
 import CostOptimizationWorkbookPage from './CostOptimizationWorkbookPage';
+import ToolsLandingPage from './ToolsLandingPage';
 
+import './App.css';
 
 function App() {
   return (
+
     <Router>
-      <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/bicep-registry" element={<BicepRegistryModulesPage />} />
+          <Route path="/hubs" element={<FinOpsHubsPage />} />
+          <Route path="/open-data" element={<OpenDataPage />} />
+          <Route path="/optimization-engine" element={<AzureOptimizationEnginePage />} />
+          <Route path="/power-bi" element={<PowerBIReportsPage />} />
+          <Route path="/powershell" element={<PowerShellModulePage />} />
           <Route path="/tools" element={<ToolsLandingPage />} />
-          <Route path="/tools/finops-hubs" element={<FinOpsHubsPage />} />
-          <Route path="/tools/power-bi-reports" element={<PowerBIReportsPage />} />
-          <Route path="/tools/cost-optimization-workbook" element={<CostOptimizationWorkbookPage />} />
-          <Route path="/tools/finops-workbooks" element={<FinOpsWorkbooksPage />} />
-          <Route path="/tools/governance-workbook" element={<GovernanceWorkbookPage />} />
-          <Route path="/tools/azure-optimization-engine" element={<AzureOptimizationEnginePage />} />
-          <Route path="/tools/powershell-module" element={<PowerShellModulePage />} />
-          <Route path="/tools/bicep-registry-modules" element={<BicepRegistryModulesPage />} />
-          <Route path="/tools/open-data" element={<OpenDataPage />} />
+          <Route path="/workbooks" element={<FinOpsWorkbooksPage />} />
+          <Route path="/workbooks/governance" element={<GovernanceWorkbookPage />} />
+          <Route path="/workbooks/optimization" element={<CostOptimizationWorkbookPage />} />
         </Routes>
-      </div>
     </Router>
   );
 };
