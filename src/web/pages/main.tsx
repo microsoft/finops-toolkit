60823346c1ab1df5a6414f92156c090d8762fee6
akiskips <root@LAPTOP-NOKG0JU9>
2024-10-26 12:28:04 +0200

    removed the normalize.css package. Added the reset rule at the start of index.css

--- src/web/pages/main.tsx
+++ src/web/pages/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import 'normalize.css';
 import '../styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
