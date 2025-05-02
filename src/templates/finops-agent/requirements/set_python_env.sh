f13fb60bf476d7af53b70373acc7d883aea626ed
msbrett <msbrett@users.no-reply.github.com>
2025-05-02 08:54:53 -0700

    docker build optimizations

--- src/templates/finops-agent/requirements/set_python_env.sh
+++ src/templates/finops-agent/requirements/set_python_env.sh
@@ -2,11 +2,12 @@
 PYTHON_ENV=$1
 apt update
 apt install libodbc2
+sudo apt autoremove 
+sudo apt clean
 
 python3 -m venv /opt/$PYTHON_ENV  \
         && export PATH=/opt/$PYTHON_ENV/bin:$PATH \
         && echo "source /opt/$PYTHON_ENV/bin/activate" >> ~/.bashrc
 
 source /opt/$PYTHON_ENV/bin/activate
-pip3 install tavily-python pyodbc
 pip3 install -r ./requirements/requirements.txt
